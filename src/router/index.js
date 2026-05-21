e346e1fdf1aba752e89428a0d5fb6fb4f606b5e6
zxd2001 <53988574+zxd2001@users.noreply.github.com>
2026-05-21 12:10:57 +0800

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -58,7 +58,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) return savedPosition
